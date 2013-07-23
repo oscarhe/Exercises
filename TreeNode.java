9d314d108a7b8bf1cd47b7be81c53ac2d696be15
Oscar He <oscarhe91@gmail.com>
2013-07-22 21:13:52 -0400

    Simple Tree data structure for testing.

---
+++ TreeNode.java
@@ -0,0 +1,15 @@
+// Generic TreeNode class for testing
+
+public class TreeNode {
+
+    public Object data;
+    public TreeNode left;
+    public TreeNode right;
+        
+    public TreeNode(Object data) {
+
+        this.data = data;
+
+    }
+    
+}
